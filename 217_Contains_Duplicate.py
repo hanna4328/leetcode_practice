7371e284d49e6cea8741a56d21fdd84841c66f96
hanna4328 <hannalily4328@gmail.com>
2026-07-12 15:19:35 +0530

    Sync: 217. Contains Duplicate (Easy) - Leet2Git

--- 217_Contains_Duplicate.py
+++ 217_Contains_Duplicate.py
@@ -1,4 +1,4 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         temp=set(nums)
-        return False if len(temp) == len(nums) else True
+        return len(nums) != len(temp)
