4a5e35637c21ce77d690c0ed5d531ec26646a3e1
hanna4328 <hannalily4328@gmail.com>
2026-07-12 15:59:58 +0530

    Sync: 1. Two Sum (Easy) - Leet2Git

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,8 +1,8 @@
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        total=0
-        runningSum=[]
-        for num in nums:
-            total+=num
-            runningSum.append(total)
-        return runningSum
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d={}
+        for index,num in enumerate(nums):
+            needed = target - num
+            if needed in d:
+                return [index,d[needed]]
+            d[num]= index
