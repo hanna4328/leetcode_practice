a48a834813d3d56dd40acb225e2629eab0ad23d1
hanna4328 <hannalily4328@gmail.com>
2026-07-19 14:28:39 +0530

    Sync: 1672. Richest Customer Wealth (Easy) - Leet2Git

--- 1672_Richest_Customer_Wealth.py
+++ 1672_Richest_Customer_Wealth.py
@@ -1,11 +1,10 @@
 class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        result=[]
-        maximum=max(candies)
-        for num in candies:
-            if num + extraCandies >= maximum:
-                result.append(True)
-            else:
-                result.append(False)
-        return result
-
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        maximum=-1
+        for customer in accounts:
+            total=0
+            for money in customer:
+                total+=money
+            if total>maximum:
+                    maximum = total
+        return maximum
